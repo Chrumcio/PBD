7ec2cf3a7e823280a0fbda1ddaef4e6ce201c65c
mateusz.koza <32997806+Chrumcio@users.noreply.github.com>
2019-11-02 13:10:04 +0100

    Remove useless folder

--- putDataToDatabase/src/main/java/com/example/putDataToDatabase/Repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.putDataToDatabase.Repository;
-
-import com.example.putDataToDatabase.Entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car,Long> {
-}
