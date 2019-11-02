5c733f9be8380fdc24f28e626823b853e7c92634
mateusz.koza <32997806+Chrumcio@users.noreply.github.com>
2019-11-02 09:58:02 +0100

    Created empty project

---
+++ putDataToDatabase/src/main/java/com/example/putDataToDatabase/PutDataToDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.putDataToDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutDataToDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PutDataToDatabaseApplication.class, args);
+	}
+}
