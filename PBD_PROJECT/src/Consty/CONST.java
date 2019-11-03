d40ca58e306700bcb1a60dc9a07bb0dff1829155
Mateusz Ludtke <sirragh97@gmail.com>
2019-11-03 19:46:59 +0100

    Moved all textfiles from .src to .textfile and changed paths in Consty

--- PBD_PROJECT/src/Consty/CONST.java
+++ PBD_PROJECT/src/Consty/CONST.java
@@ -1,17 +1,17 @@
 package Consty;
 
 public class CONST {
-	public static String namesUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\names.txt";
-	public static String surnamesUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\surnames.txt";
-	public static String streetsUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\streets.txt";
-	public static String citiesUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\cities.txt";
-	public static String specjalizationsUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\specjalizations.txt";
-	public static String diagnosisUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\diagnosis.txt";
-	public static String instrumentsUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\instruments.txt";
-	public static String mediciensUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\mediciens.txt";
-	public static String providerUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\provider.txt";
-	public static String statusUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\status.txt";
-	public static String typeOfVisitsUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\typeOfVisits.txt";
+	public static String namesUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\names.txt";
+	public static String surnamesUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\surnames.txt";
+	public static String streetsUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\streets.txt";
+	public static String citiesUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\cities.txt";
+	public static String specjalizationsUrl="D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\specjalizations.txt";
+	public static String diagnosisUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\diagnosis.txt";
+	public static String instrumentsUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\instruments.txt";
+	public static String mediciensUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\mediciens.txt";
+	public static String providerUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\provider.txt";
+	public static String statusUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\status.txt";
+	public static String typeOfVisitsUrl = "D:\\github\\PBD\\PBD_PROJECT\\src\\textfile\\typeOfVisits.txt";
 	
 	
 	public static String url = "jdbc:postgresql://localhost/postgres";
