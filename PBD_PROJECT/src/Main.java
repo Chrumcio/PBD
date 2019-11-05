880f772d582a12806ff5f347f9e10a710fd02ddf
mateusz.koza <32997806+Chrumcio@users.noreply.github.com>
2019-11-05 01:06:16 +0100

    added method to add data to database

--- PBD_PROJECT/src/Main.java
+++ PBD_PROJECT/src/Main.java
@@ -1,100 +1,79 @@
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
 import Consty.CONST;
+import Entity.Diagnostyka;
 import Entity.Lekarstwo;
+import Entity.Przyrzad_Medyczny;
 import datafiles.Medicines;
 import draw.*;
 
 
 public class Main {
+    private static List<String> listOfCodes = new ArrayList<>();
     private static List<String> listOfNames = new ArrayList<>();
+    private static List<String> listOfInstumentsName = new ArrayList<>();
 
-    public static List<String> setListOfNames() throws IOException {
-        File file = new File(CONST.namesUrl);
+    public static void setListOfDiagnosis() throws IOException {
+        File file = new File(CONST.diagnosisUrl);
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
-        String name;
-        while((name = reader.readLine()) != null){
-            listOfNames.add(name);
-            System.out.println(name); 
+        String line;
+        while((line = reader.readLine()) != null){
+            listOfCodes.add(line.substring(0,4));
+            listOfNames.add(line.substring(4));
         }
-        
-        return listOfNames;
+    }
+
+    public static void setListOfInstruments() throws IOException{
+        File file = new File(CONST.instrumentsUrl);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
+        String line;
+        while((line = reader.readLine()) != null){
+            listOfInstumentsName.add(line);
+        }
     }
 
     public static void main(String[] args) throws SQLException, IOException {
         String url = CONST.url;
         String user = CONST.user;
         String password = CONST.haslo;
-
-
         Connection connection = DriverManager.getConnection(url,user,password);
-//        Medicines medicines = new Medicines();
-//        List<String> medicinesList = medicines.getListOfMedicines();
-//        for(int i=0;i<medicinesList.size();i++){
-//            Lekarstwo lekarstwo = new Lekarstwo();
-//            lekarstwo.setId(i);
-//            int a = (int)(Math.random()*medicinesList.size());
-//            lekarstwo.setNazwa(medicinesList.get(a));
-//            lekarstwo.addLekarstwo(connection);
-//        }
-        try (
-             PreparedStatement pst = connection.prepareStatement("SELECT * FROM dostawca");
-             ResultSet rs = pst.executeQuery()) {
-            while (rs.next()) {
-                System.out.print(rs.getInt(1));
-                System.out.print(": ");
-                System.out.println(rs.getString(2));
+
+//        DrawRoom drawRoom = new DrawRoom(url, user, password);
+//        drawRoom.addRoom(80);
+//        DoctorDraw doctorDraw = new DoctorDraw(url,user,password);
+//        doctorDraw.addDoctor(100);
+//        MedicineDraw medicineDraw = new MedicineDraw(url,user,password);
+//        medicineDraw.addMedicine(200);
+//        NurseDraw nurseDraw = new NurseDraw(url,user,password);
+//        nurseDraw.addNurse(50);
+//        PatientDraw patientDraw = new PatientDraw(url,user,password);
+//        patientDraw.addPatient(50000);
+//        ProviderDraw providerDraw = new ProviderDraw(url,user,password);
+//        providerDraw.addProvider(20);
+//        RecepcionistsDraw.randRecepcjonistka(20);
+        setListOfDiagnosis();
+        for(int i=0;i<800;i++) {
+            Diagnostyka diagnostyka = new Diagnostyka();
+            int rand = (int) (Math.random()*899)+1;
+            try {
+                diagnostyka.setIdDiagnostyki(listOfCodes.get(rand));
+                diagnostyka.setNazwaDiagnostyki(listOfNames.get(rand));
+                diagnostyka.addDiagnostyka(connection);
+            }catch (Exception e){
+
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
         }
-        
-        System.out.println("XXXXXXXXXXXXXXX");
-        
-        
-        
-        //RecepcionistsDraw.addRecepcjonistka(30, url, user, password);
-        //MedicineDraw medicineDraw = new MedicineDraw(url, user, password);
-        //medicineDraw.addMedicine(10);
-        //PatientDraw patientDraw = new PatientDraw(url, user, password);
-        //patientDraw.addPatient(10);
-        //DoctorDraw  doctorDraw = new DoctorDraw(url, user, password);
-        //doctorDraw.addDoctor(10);
-        //NurseDraw nurseDraw = new NurseDraw(url, user, password);
-        //nurseDraw.addNurse(10);
-        //ProviderDraw providerDraw = new ProviderDraw(url, user, password);
-        //providerDraw.addProvider(10);
-        DrawRoom drawRoom = new DrawRoom(url, user, password);
-        drawRoom.addRoom(10);
-//
-        
-        
-        
-        
-        
-        
-        
-        
-//
-//        int id = 6;
-//        String author = "Trygve Gulbranssen";
-//        String query = "INSERT INTO car(idcar,name) VALUES(?,?)";
-//
-        
-        
-        
-//        try (Connection con = DriverManager.getConnection(url, user, password);
-//             PreparedStatement pst = con.prepareStatement(query)) {
-//            pst.setInt(1, id);
-//            pst.setString(2, author);
-//            pst.executeUpdate();
-//
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        }
+        setListOfInstruments();
+        for(int i=0;i<listOfInstumentsName.size();i++){
+            Przyrzad_Medyczny przyrzadMedyczny = new Przyrzad_Medyczny();
+            przyrzadMedyczny.setId(i);
+            przyrzadMedyczny.setNazwa(listOfInstumentsName.get((int)Math.random()*listOfInstumentsName.size()));
+            przyrzadMedyczny.addPrzyrzad_Medyczny(connection);
+        }
     }
 }
